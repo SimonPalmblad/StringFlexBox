1282aee8021b22c64142f1647bd20dab2e39c103
SimonPalmblad <simon.palmblad@live.se>
2025-07-24 18:23:28 +0200

    Updated demo file for release.

--- StringFlexBox/_Demo.cs
+++ StringFlexBox/_Demo.cs
@@ -1,12 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more 
+﻿
+/* 
+ * This is a demo for the StringFlexBox library.
+ * - Here you can see how to use the various features and functions available with examples of how they are rendered. 
+ * - Choose the demo(s) to display by uncommenting their respective definition below.
+ * - WIP and experimental features have not tested and may not work as intended or cause errors.
+ */
 
-//#define printTextboxDemo
-//#define printBorderDemo
+// ______ DEMO DEFINITIONS ______ //
+#define printTextboxDemo
 //#define printBorderlessDemo
+//#define printBorderDemo
 //#define printCustomPaddingDemo
 
-//#define printAddingContentDemo
-#define printResizingDemo
+// ______ WIP & EXPERIMENTAL FEATURES ______ //
+//#define printResizingDemo
 
 using TextboxRebuild.HelperClasses;
 
@@ -22,81 +29,31 @@ TextBox textbox3 = new(textShort1, 20, 3);
 TextBox textbox4 = new(textShort2, 20, 5);
 TextBox textbox5 = new(textShort3, 30, 5);
 
-#region WIP - Resizing
-#if printResizingDemo
-
-Console.WriteLine(textbox3);
-int resize1 = 25;
-textbox3.Resize(resize1);
-
-Console.WriteLine($"Resized to: {resize1}");
-Console.WriteLine(textbox3);
-
-int resize2 = 27;
-Console.WriteLine($"Resized to: {resize2}");
-textbox3.Resize(resize2);
-
-Console.WriteLine(textbox3);
-
-#endif
-#endregion
-
-#region WIP - Affix/Suffixing
-#if printAddingContentDemo
-    PrintBoxDebug.PrintWithDimensions(textbox3);
-
-    textbox2.AddLineSuffix(" ~yo");
-    PrintBoxDebug.PrintWithDimensions(textbox2);
-
-    textbox3.AddLinePrefix("1. ");
-    PrintBoxDebug.PrintWithDimensions(textbox3);
-
-    // Prefixing each row with an incrementing number
-    for (int i = 0; i < textbox5.Texts().Count; i++)
-    {
-        textbox5.AddPrefixAtLine((i + 1).ToString() + ". ", i);
-    }
-    
-    PrintBoxDebug.PrintWithDimensions(textbox5);
-
-    //Console.WriteLine(textbox1.Texts().Count);
-    
-//for (int i = 0; i < textbox1.Texts().Count; i++)
-//    {
-//        textbox1.AddPrefixAtLine((i + 1).ToString() + ". ", i);
-//    }
-
-    //PrintBoxDebug.PrintWithDimensions(textbox1);
-
-//FlexBoxHorizontal horizontalBoxes = new(new List<StringFlexBox> { textbox2, textbox3 }, new Padding(5));
-//PrintBoxDebug.PrintWithDimensions(horizontalBoxes);
-
-//FlexBoxVertical verticalBoxes = new(new List<StringFlexBox> { textbox3, textbox2 }, new Padding(2));
-//PrintBoxDebug.PrintWithDimensions(verticalBoxes);
 
 
-#endif
-#endregion
+
 
 #region GitHub README Demos
 
 #region TextBox DEMO
-#if printTextBoxDemo
-    string textDemoBasic = "This a string inside of a textbox with automatic word wrap.";
-    TextBox textBoxDemoBasic = new(textDemoBasic, 20, 0);
 
-    string textDemoPadding = "The text can also have padding added to the box. Here's with a padding of 3.";
-    TextBox textBoxDemoPadding = new(textDemoPadding, 20, 3);
+#if printTextboxDemo
+string textDemoBasic = "This a string inside of a textbox with automatic word wrap.";
+TextBox textBoxDemoBasic = new(textDemoBasic, 20, 0);
 
-    string textDemoSize = "You can set a specific size for your textbox. This is double the size of the previous ones.";
-    TextBox textBoxDemoSize = new(textDemoSize, 40, 3);
+string textDemoPadding = "The text can also have padding added to the box. Here's with a padding of 3.";
+TextBox textBoxDemoPadding = new(textDemoPadding, 20, 3);
 
-    FlexBoxHorizontal horizontalBoxes = new(new List<StringFlexBox> { textBoxDemoBasic, textBoxDemoPadding }, new Padding(5));
-    FlexBoxVertical verticalBoxes = new(new List<StringFlexBox> { textBoxDemoBasic, textBoxDemoPadding }, new Padding(2));
+string textDemoSize = "You can set a specific size for your textbox. This is double the size of the previous ones.";
+TextBox textBoxDemoSize = new(textDemoSize, 40, 3);
 
-    FlexBoxHorizontal horizontalVerticalBoxes = new(new List<StringFlexBox> { verticalBoxes, horizontalBoxes }, new Padding(2));
-    Console.WriteLine(horizontalVerticalBoxes.FormattedText);   
+FlexBoxHorizontal horizontalBoxes = new(new List<StringFlexBox> { textBoxDemoBasic, textBoxDemoPadding }, new Padding(5));
+FlexBoxVertical verticalBoxes = new(new List<StringFlexBox> { textBoxDemoBasic, textBoxDemoPadding }, new Padding(2));
+
+FlexBoxHorizontal horizontalVerticalBoxes = new(new List<StringFlexBox> { verticalBoxes, horizontalBoxes }, new Padding(2));
+Console.WriteLine(horizontalVerticalBoxes.FormattedText);
 #endif
+
 #endregion
 
 #region Borderless DEMO
@@ -112,6 +69,7 @@ Console.WriteLine(textbox3);
     Console.WriteLine(horizontalBorderless.FormattedText); 
     
 #endif
+
 #endregion
 
 #region Custom border DEMO
@@ -132,10 +90,13 @@ Console.WriteLine(textbox3);
     FlexBoxHorizontal horizontalCustomBorder = new(new List<StringFlexBox> { textBoxCustomBorder1, textBoxCustomBorder2 }, 5, customBorder);
 
     Console.WriteLine(horizontalCustomBorder.FormattedText); 
+
 #endif
+
 #endregion
 
 #region Custom Padding DEMO
+
 #if printCustomPaddingDemo
     string customPaddingText1 = "This is a textbox with horizontal padding 3 and vertical padding 10.";
     TextBox customPaddingTextBox1 = new(customPaddingText1, 20, new Padding(horizontalPadding: 3, verticalPadding: 10, centerPadding: 0));
@@ -148,6 +109,28 @@ Console.WriteLine(textbox3);
     Console.WriteLine(horizontalCustomPadding);
 
 #endif
+
 #endregion
 
+#endregion
+
+#region WIP & Experimental
+
+#if printResizingDemo
+#region Resizing
+Console.WriteLine(textbox3);
+int resize1 = 25;
+textbox3.Resize(resize1);
+
+Console.WriteLine($"Resized to: {resize1}");
+Console.WriteLine(textbox3);
+
+int resize2 = 27;
+Console.WriteLine($"Resized to: {resize2}");
+textbox3.Resize(resize2);
+
+Console.WriteLine(textbox3); 
+#endregion
+
+#endif
 #endregion
